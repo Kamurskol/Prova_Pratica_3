6b08faabda3ec847d0374d7af5c0cbad2cc4e0a0
Marcos <97557948+Kamurskol@users.noreply.github.com>
2022-06-30 09:01:01 -0300

    Update Questao 1.py

--- Questao 1.py
+++ Questao 1.py
@@ -2,7 +2,7 @@ import re # Aqui é importada a função re, que permite trabalhar com expressõ
 
 str = str(input('Digite algo: ')) #Aqui será digitada a string
 
-novastring = re.sub('[\s\d]', '' ,str) #A função re.sub irá substituir \s (espaços em branco) e \d (números) por '' (vazio)
+novastring = re.sub('[\s\d]', '' ,str) #A função re.sub irá substituir \s (espaços em branco) e \d (números/dígitos) por '' (vazio)
 
 nova = novastring.replace('a', 'A') # Aqui é criada uma nova string utilizando a string onde foram retirados os números e espaços em branco, nela todo 'a' minúsculo é substituido por um 'A' maiúsculo
 nova = nova.replace('e', 'E') # Aqui é usada a lista que foi recém-criada, nela trocamos os 'e's minúsculos por 'E's maiúsculos

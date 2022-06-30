e57cfe2f577a12fc883e7d702bb96b0b814ca13b
Marcos <97557948+Kamurskol@users.noreply.github.com>
2022-06-30 08:59:06 -0300

    Update Questao_2_referenciaAutor.py

--- Questao_2_referenciaAutor.py
+++ Questao_2_referenciaAutor.py
@@ -6,5 +6,5 @@ def referenciaAutor(str):# Definida a função de referência, com classe string
     autor = nmlista[0:-1]# Essa lista contém o nome do autor, menos o último sobrenome
 
     print(f'{nmlista[-1].upper()},', *autor)# 'nmlista[-1].upper()' vai imprimir o último elemento da lista que contém o nome - o último sobrenome - em caixa alta
-                                            # O '*' imprime a lista sem os colchetes
+                                            # O '*' imprime a lista autor (que não contém o último sobrenome) sem os colchetes
 referenciaAutor(str)# Aqui é chamada a função de referência
